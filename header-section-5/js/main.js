d48ca6660814e770895de80887e8621fb6a0ee18
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-07 09:20:33 +0600

    Add files via upload

---
+++ header-section-5/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+		$(".home-page-slides").owlCarousel({
+
+			items:1,
+			loop:true,
+			autoplay:true,
+			nav:true,
+			navText:["<i class='fa fa-long-arrow-left'>","<i class='fa fa-long-arrow-right'>"],
+		});
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
