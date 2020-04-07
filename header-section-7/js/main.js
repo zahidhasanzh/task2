8cb3a7daccd1214abd3cc3199cd327714b04031c
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-07 09:21:18 +0600

    Add files via upload

---
+++ header-section-7/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".welcome-area").YTPlayer({
+
+		fitToBackground: true,
+    	videoId: 'LSmgKRx5pBo'
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
