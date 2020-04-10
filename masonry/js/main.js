022c9f21cba61658de229f5b2c9d742e2d06bf14
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-10 10:56:35 +0600

    Add files via upload

---
+++ masonry/js/main.js
@@ -0,0 +1,17 @@
+  
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     $(".portfolio-list").masonry({
+        horizontalOrder: true
+     });
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
