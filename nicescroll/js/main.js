da690f7b90f1347f3ba740756f9dea55fcac313c
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-10 10:58:04 +0600

    Add files via upload

---
+++ nicescroll/js/main.js
@@ -0,0 +1,17 @@
+
+
+$(function() {  
+    $("body").niceScroll({
+    	cursorcolor:"red",
+    	cursorwidth: "10px",
+    	autohidemode: false,
+    	cursoropacitymax: .8,
+    });
+
+    $(".custom-scrollbar").niceScroll({
+ 		cursorcolor:"blue",
+ 		autohidemode: false,
+    });
+
+
+});
