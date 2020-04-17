78b14f0c65e07de4c25485bb479c96f18b63e67f
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-17 09:10:18 +0600

    Add files via upload

---
+++ video-popup-2/js/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+	$(".video-play-btn").modalVideo();
+
+	$(".video-play-btn").on('click', function(){
+
+		return false;
+	});
+
+});
+
+
+
+
+
+
+$(".js-modal-btn").modalVideo();
