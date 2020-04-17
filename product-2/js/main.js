4973deb6fa187e1710449c2571043a279322d5fd
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-17 09:01:04 +0600

    Add files via upload

---
+++ product-2/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".product-carousel").owlCarousel({
+
+		items:4,
+		nav:true,
+		navText:["<i class='fa fa-angle-left'>","<i class='fa fa-angle-right'>"],
+		loop:true,
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
