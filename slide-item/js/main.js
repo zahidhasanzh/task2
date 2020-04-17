5dd61fa0cc0d33f0dc5f53077839626baacc5519
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-17 09:05:14 +0600

    Add files via upload

---
+++ slide-item/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".portfolio-items").owlCarousel({
+
+		items:5,
+		loop:true,
+		nav:false,
+		dots:true,
+	})
+
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
