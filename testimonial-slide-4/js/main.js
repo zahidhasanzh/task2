86c22ea0ecfc85ec31e06ab92eedb34646da88fb
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-04-17 09:08:01 +0600

    Add files via upload

---
+++ testimonial-slide-4/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".team-carousel").owlCarousel({
+		items:3,
+		margin:30,
+		nav:true,
+		loop:true,
+		navText:["<i class='fa fa-angle-left'>","<i class='fa fa-angle-right'>"],
+
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
